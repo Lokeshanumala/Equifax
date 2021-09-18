8bf9a70c6fe62d6a24ec61fd01c88a15d59337a9
A LOKESH <A LOKESH@192.168.1.37>
2021-09-18 15:23:18 +0530

    Updated Cucumber extended reports

--- qa.equifax.com/src/test/java/com/cucumberRuner/Runnertest.java
+++ qa.equifax.com/src/test/java/com/cucumberRuner/Runnertest.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 
     @RunWith(Cucumber.class)
-    @CucumberOptions(features="src/test/resources/features/",glue={"stepDefinition"})
+    @CucumberOptions(features="src/test/resources/features/",glue={"stepDefinition"},
+//    		plugin = { "pretty", "html:target/cucumber-reports" },
+//    		monochrome = true)
 
-
+plugin={"com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html"})
 public class Runnertest {
 	
 
